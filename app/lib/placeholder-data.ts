e662d48e64bb52d391e38b5840e47ab6abfc2199
Sayantan Paul <82387944+sayantan-paul-007@users.noreply.github.com>
2025-05-13 21:21:39 +0530

    Cleaned the jargons in placeholder-data.ts

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,5 +1,3 @@
-// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
-// https://nextjs.org/learn/dashboard-app/fetching-data
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
@@ -8,7 +6,6 @@ const users = [
     password: '123456',
   },
 ];
-
 const customers = [
   {
     id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
@@ -47,7 +44,6 @@ const customers = [
     image_url: '/customers/balazs-orban.png',
   },
 ];
-
 const invoices = [
   {
     customer_id: customers[0].id,
@@ -128,7 +124,6 @@ const invoices = [
     date: '2022-06-05',
   },
 ];
-
 const revenue = [
   { month: 'Jan', revenue: 2000 },
   { month: 'Feb', revenue: 1800 },
@@ -143,5 +138,4 @@ const revenue = [
   { month: 'Nov', revenue: 3000 },
   { month: 'Dec', revenue: 4800 },
 ];
-
 export { users, customers, invoices, revenue };
